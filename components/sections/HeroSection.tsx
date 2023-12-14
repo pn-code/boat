13b995ea4e3c78486922fd5b724808fc7ad352f1
Philip Nguyen <philipnguyen953@gmail.com>
2023-12-13 16:42:16 -0800

    Updated bg-img so it appears correctly on mobile

--- components/sections/HeroSection.tsx
+++ components/sections/HeroSection.tsx
@@ -3,19 +3,19 @@ import React from "react";
 export default function HeroSection() {
   return (
     <div id="hero">
-      <div className="w-full min-h-[70vh] lg:min-h-[100vh] md:pb-[300px] flex flex-col items-center relative bg-gradient-to-b from-[#01142b]/[30%] to-[#01142b]">
-        <div className="w-full flex flex-col items-center gap-1 z-10 mt-20 md:mt-[16%]">
-          <p className="text-center sm:text-xl lg:text-2xl tracking-wide font-semibold text-orange-400">
+      <div className="w-full min-h-[80vh] lg:min-h-[100vh] md:pb-[300px] flex flex-col items-center relative bg-gradient-to-b from-[#01142b]/[30%] to-[#01142b]">
+        <div className="w-full flex flex-col items-center gap-4 z-10 mt-24 md:mt-[14%]">
+          <p className="text-center text-xl md:text-2xl lg:text-3xl tracking-tight font-bold text-orange-400">
             California State University, Fullerton
           </p>
-          <h1 className="text-lg sm:text-2xl lg:text-5xl font-bold tracking-wide text-white">
-            The Hydrogen-Based Boat Project
+          <h1 className="text-2xl md:text-4xl lg:text-6xl font-bold tracking-tight text-white my-0 py-0">
+            The Hydrogen-Based Boat
           </h1>
+          <p className="text-[16px] md:text-lg max-w-[300px] sm:max-w-[400px] lg:max-w-[600px] text-center text-slate-300 font-semibold tracking-wide z-10 my-2">
+            Revolutionizing Water Travel: Innovative Hydrogen-Powered Boat
+            Engineering
+          </p>
         </div>
-        <p className="text-[16px] max-w-[300px] sm:max-w-[400px] lg:max-w-[600px] text-center mt-2 lg:mt-6 text-slate-200 font-semibold tracking-wide z-10">
-          Revolutionizing Water Travel: Innovative Hydrogen-Powered Boat
-          Engineering
-        </p>
       </div>
     </div>
   );
