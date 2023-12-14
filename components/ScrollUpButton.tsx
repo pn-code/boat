24fc1213adfc8b82fb93af936cd51d61f01ac065
Philip Nguyen <philipnguyen953@gmail.com>
2023-12-13 16:42:39 -0800

    Updated pb to extend component through full height

--- components/ScrollUpButton.tsx
+++ components/ScrollUpButton.tsx
@@ -3,7 +3,7 @@ import { ChevronsUp } from "lucide-react";
 
 export default function ScrollUpButton() {
   return (
-    <div className="w-full flex justify-center lg:pt-12 pb-8 bg-[#01142b]">
+    <div className="w-full flex justify-center lg:pt-12 pb-4 bg-[#01142b]">
       <div className="flex flex-col items-center gap-4">
         <a
           className="p-4 bg-slate-200 rounded-full hover:bg-blue-300/80 duration-100 ease-in"
