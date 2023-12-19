fe688cf044d12fe66370b1899852106f6fc5e265
Philip Nguyen <philipnguyen953@gmail.com>
2023-12-18 23:10:37 -0800

    Updated concept content to have more pop

--- components/sections/ConceptSection.tsx
+++ components/sections/ConceptSection.tsx
@@ -14,52 +14,35 @@ export default function ConceptSection() {
           </header>
         </AnimateOnScrollContainer>
 
-        <div className="w-full flex flex-col justify-center items-center mt-8">
-          <AnimateOnScrollContainer>
-            <p>
-              The Hydrogen-Powered RC Boat concept envisions a cutting-edge
-              remote-controlled watercraft that operates using hydrogen fuel
-              cells as a clean and efficient power source. The design integrates
-              advanced technology to harness the potential of hydrogen, a
-              renewable energy carrier, for propulsion. The key components
-              include a compact hydrogen fuel cell system, lightweight materials
-              for the hull, and a sophisticated control system.
-            </p>
-          </AnimateOnScrollContainer>
+        <div className="w-full flex flex-col justify-center items-center mt-8 gap-8">
+          <p className="text-2xl tracking-tight">
+            <AnimateOnScrollContainer>
+              Envision a cutting-edge remote-controlled watercraft that operates
+              using
+              <span className="text-orange-400 font-semibold">
+                {" "}
+                hydrogen fuel cells
+              </span>{" "}
+              as a
+              <span className="text-green-400 font-semibold">
+                {" "}
+                clean and efficient power source.
+              </span>
+            </AnimateOnScrollContainer>
+          </p>
 
-          <AnimateOnScrollContainer>
-            <p>
-              The hydrogen fuel cell system comprises a proton exchange membrane
-              (PEM) fuel cell stack, a hydrogen storage tank, and an onboard
-              converter to generate electricity. The fuel cell stack efficiently
-              converts hydrogen into electrical energy, emitting only water as a
-              byproduct. The lightweight hull is constructed from durable and
-              eco-friendly materials to enhance buoyancy and speed.
-              Additionally, the boat features a streamlined and aerodynamic
-              design to optimize energy efficiency.
-            </p>
-          </AnimateOnScrollContainer>
-
-          <AnimateOnScrollContainer>
-            <p>
-              The control system is equipped with a user-friendly remote
-              interface, allowing enthusiasts to navigate the RC boat with
-              precision. Real-time data feedback on hydrogen levels, battery
-              status, and performance metrics enhances the user experience.
-              Safety measures, including automatic shut-off in the event of
-              malfunctions or low hydrogen levels, are integrated to ensure a
-              secure and reliable operation.
-            </p>
-          </AnimateOnScrollContainer>
-
-          <AnimateOnScrollContainer>
-            <p>
-              This Hydrogen-Powered RC Boat concept not only offers a thrilling
-              recreational experience but also promotes environmentally
-              conscious practices by showcasing the potential of hydrogen as a
-              clean energy solution for marine applications.
-            </p>
-          </AnimateOnScrollContainer>
+          <p className="text-2xl tracking-tight">
+            <AnimateOnScrollContainer>
+              <span className="text-orange-400 font-semibold">
+                By integrating advanced technology
+              </span>{" "}
+              to harness the potential of hydrogen, a renewable energy carrier,{" "}
+              <span className="text-green-400 font-semibold">
+                we can create the capacity for propulsion using renewable
+                resources.
+              </span>
+            </AnimateOnScrollContainer>
+          </p>
         </div>
       </div>
     </>
