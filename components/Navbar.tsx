3d132dd1fcfe53e40d91b9bfd7a873a3dda42df8
Philip Nguyen <philipnguyen953@gmail.com>
2023-12-21 13:10:14 -0800

    Updated navbar styles for wide tablets

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -16,7 +16,7 @@ const links: string[] = [
 
 export default function Navbar() {
   return (
-    <nav className="w-full h-[70px] z-20 fixed top-0 flex lg:justify-center justify-between items-center gap-4 lg:gap-12 px-5 lg:px-8 py-4 bg-slate-900 text-white">
+    <nav className="w-full h-[70px] z-20 fixed top-0 flex 2xl:justify-center justify-between items-center gap-4 lg:gap-12 px-5 lg:px-8 py-4 bg-slate-900 text-white">
       {/* Logo */}
       <header className="flex hover:scale-105 duration-100">
         <a href="/">
@@ -25,7 +25,7 @@ export default function Navbar() {
       </header>
 
       {/* Navigation Links on Larger Devices*/}
-      <ul className="hidden lg:flex justify-center md:gap-12 text-[14px] tracking-widest font-semibold py-1.5 text-white">
+      <ul className="hidden lg:flex justify-center gap-5 2xl:gap-10 text-[14px] tracking-widest font-semibold py-1.5 text-white">
         {links.map((link) => (
           <a
             className="pt-1 uppercase border-b-2 border-transparent hover:border-orange-500 duration-200 ease-linear"
