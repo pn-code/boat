100fe0a4a47fe3ef1a105850d2deaf1bbe3b8f8e
Philip Nguyen <philipnguyen953@gmail.com>
2023-12-22 12:06:54 -0800

    Simplified transitions & added transition-out

--- components/MobileNavbar.tsx
+++ components/MobileNavbar.tsx
@@ -26,26 +26,28 @@ export default function MobileNavbar({ links }: MobileNavbarProps) {
       </button>
 
       {/* Menu Wrapper */}
-      {isOpen && (
-        <div
-          className={`fixed z-20 top-[70px] left-0 bg-slate-900 w-full min-h-[calc(100vh-70px)] animate-slide-in-left`}
-        >
-          {/* Menu List */}
-          <ul className="flex flex-col gap-2 font-semibold w-full h-full pt-4 text-3xl">
-            {links.map((link) => (
-              <li className="flex w-full px-7" key={link}>
-                <a
-                  onClick={handleOpenMenu}
-                  className="py-1 uppercase duration-200 ease-linear hover:text-orange-400 hover:-translate-y-0.5"
-                  href={`#${link}`}
-                >
-                  {link}
-                </a>
-              </li>
-            ))}
-          </ul>
-        </div>
-      )}
+      <div
+        className={
+          isOpen
+            ? "fixed z-20 top-[70px] left-0 bg-slate-900 w-full min-h-[calc(100vh-70px)] transition-in"
+            : "fixed z-20 top-[70px] left-0 bg-slate-900 w-full min-h-[calc(100vh-70px)] transition-out"
+        }
+      >
+        {/* Menu List */}
+        <ul className="flex flex-col gap-2 font-semibold w-full h-full pt-4 text-3xl">
+          {links.map((link) => (
+            <li className="flex w-full px-7" key={link}>
+              <a
+                onClick={handleOpenMenu}
+                className="py-1 uppercase duration-200 ease-linear hover:text-orange-400 hover:-translate-y-0.5"
+                href={`#${link}`}
+              >
+                {link}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
